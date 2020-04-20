095dee0f9d2532fb6c99fe6b3b753b69d6e67239
Lim Jing Heng <limjh16@gmail.com>
2020-04-20 20:05:54 +0800

    add the commit SHA to the commit message
    makes it easier to check which version is currently deployed

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,7 @@ git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
-git commit -m 'jekyll build from Action' && \
+git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
 git push --force $remote_repo master:$remote_branch && \
 rm -fr .git && \
 cd ..
