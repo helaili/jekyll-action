af3293c2d6f25c063e2a3448433fe47d4d65b11c
limjh16 <limjh16@gmail.com>
2020-04-23 10:37:59 +0800

    fix echo debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,13 @@ echo "::debug ::Completed bundle install"
 
 if [[ ${INPUT_JEKYLL_SRC} ]]; then
   JEKYLL_SRC=${INPUT_JEKYLL_SRC}
-  echo "::debug ::Using parameter value ${INPUT_JEKYLL_SRC} as a source directory"
+  echo "::debug ::Using parameter value ${JEKYLL_SRC} as a source directory"
 elif [[ ${SRC} ]]; then
   JEKYLL_SRC=${SRC}
-  echo "::debug ::Using SRC environment var value ${INPUT_JEKYLL_SRC} as a source directory"
+  echo "::debug ::Using SRC environment var value ${JEKYLL_SRC} as a source directory"
 else
   JEKYLL_SRC=$(find . -path ./vendor/bundle -prune -o -name '_config.yml' -exec dirname {} \;)
-  echo "::debug ::Resolved ${INPUT_JEKYLL_SRC} as a source directory"
+  echo "::debug ::Resolved ${JEKYLL_SRC} as a source directory"
 fi
 
 bundle exec jekyll build -s ${JEKYLL_SRC} -d build
