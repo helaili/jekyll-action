ccb5e360a270cd20bdb7dc0981a5dd0966025da3
Lim Jing Heng <limjh16@gmail.com>
2020-04-25 18:11:58 +0800

    Use production environment
    https://jekyllrb.com/docs/step-by-step/10-deployment/

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ else
   echo "::debug ::Resolved ${JEKYLL_SRC} as a source directory"
 fi
 
-bundle exec jekyll build -s ${JEKYLL_SRC} -d build
+JEKYLL_ENV=production bundle exec jekyll build -s ${JEKYLL_SRC} -d build
 echo "Jekyll build done"
 
 cd build
