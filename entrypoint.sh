2fa128a2de27ba0e5fa76cb78c42a203f5ee9ab3
Alain Hélaïli <alain@carbonhub.io>
2020-05-04 09:49:05 +0200

    this should fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ fi
 
 echo "Publishing to ${GITHUB_REPOSITORY} on branch ${remote_branch}"
 
-remote_repo="https://${JEKYLL_PAT}@github.com/${GITHUB_REPOSITORY}.git" && \
+remote_repo="https://${JEKYLL_PATXXX}@github.com/${GITHUB_REPOSITORY}.git" && \
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
@@ -49,4 +49,6 @@ git add . && \
 git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
 git push --force $remote_repo master:$remote_branch && \
 rm -fr .git && \
-cd .. && exit 0 || exit 1
+cd .. 
+
+exit $?
