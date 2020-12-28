5bbb7d73a4e96c73cb6115acad19db134ed7c820
Alain Hélaïli <alain@carbonhub.io>
2020-12-28 19:04:02 +0100

    Testing jekyll_env

--- tests/e2e/cypress/integration/basic/spec.js
+++ tests/e2e/cypress/integration/basic/spec.js
@@ -2,4 +2,5 @@ it('works', () => {
   cy.visit('/jekyll-action/')
   cy.get('#basic').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Basic')
+  cy.get('#env').should('contain', 'development')
 })
--- tests/e2e/cypress/integration/jekyll_src/spec.js
+++ tests/e2e/cypress/integration/jekyll_src/spec.js
@@ -2,4 +2,5 @@ it('works', () => {
   cy.visit('/jekyll-action/')
   cy.get('#jekyll_src').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Jekyll Src')
+  cy.get('#env').should('contain', 'production')
 })
