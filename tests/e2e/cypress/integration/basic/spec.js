98719c1186648dc80d2e8a7f6bcc108568e53f43
Alain Hélaïli <alain@carbonhub.io>
2020-12-28 18:52:59 +0100

    harden the tests

--- tests/e2e/cypress/integration/basic/spec.js
+++ tests/e2e/cypress/integration/basic/spec.js
@@ -1,4 +1,5 @@
 it('works', () => {
   cy.visit('/jekyll-action/')
+  cy.get('#basic').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Basic')
 })
--- tests/e2e/cypress/integration/jekyll_src/spec.js
+++ tests/e2e/cypress/integration/jekyll_src/spec.js
@@ -1,4 +1,5 @@
 it('works', () => {
   cy.visit('/jekyll-action/')
+  cy.get('#jekyll_src').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Jekyll Src')
 })
