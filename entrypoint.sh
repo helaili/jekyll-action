b04204acbcb5731d9f327e026dfe89a31fea5d09
Alain Hélaïli <alain@carbonhub.io>
2021-02-01 22:03:05 +0100

    reorder

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,6 +81,20 @@ else
   echo "::debug::Jekyll debug is off"
 fi
 
+if [ -n "${INPUT_TARGET_BRANCH}" ]; then
+  remote_branch="${INPUT_TARGET_BRANCH}"
+  echo "::debug::target branch is set via input parameter"
+else
+  # Is this a regular repo or an org.github.io type of repo
+  case "${GITHUB_REPOSITORY}" in
+    *.github.io) remote_branch="master" ;;
+    *)           remote_branch="gh-pages" ;;
+  esac
+fi
+
+REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+echo "::debug::Remote is to ${REMOTE_REPO}"
+
 JEKYLL_ENV=${INPUT_JEKYLL_ENV} bundle exec ${BUNDLE_ARGS} jekyll build -s ${GITHUB_WORKSPACE}/${JEKYLL_SRC} -d build ${INPUT_JEKYLL_BUILD_OPTIONS} ${VERBOSE} 
 echo "Jekyll build done"
 
@@ -93,33 +107,19 @@ cd build
 # No need to have GitHub Pages to run Jekyll
 touch .nojekyll
 
-
-if [ -n "${INPUT_TARGET_BRANCH}" ]; then
-  remote_branch="${INPUT_TARGET_BRANCH}"
-  echo "::debug::target branch is set via input parameter"
-else
-  # Is this a regular repo or an org.github.io type of repo
-  case "${GITHUB_REPOSITORY}" in
-    *.github.io) remote_branch="master" ;;
-    *)           remote_branch="gh-pages" ;;
-  esac
-fi
-
 if [ "${GITHUB_REF}" = "refs/heads/${remote_branch}" ]; then
   echo "::error::Cannot publish on branch ${remote_branch}"
   exit 1
 fi
 
 echo "Publishing to ${GITHUB_REPOSITORY} on branch ${remote_branch}"
-echo "::debug::Pushing to https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-git push --force $remote_repo master:$remote_branch && \
+git push --force $REMOTE_REPO master:$remote_branch && \
 rm -fr .git && \
 cd .. 
 
