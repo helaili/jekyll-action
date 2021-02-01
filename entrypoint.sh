b74906aa5730083d07c3afd59741dfd57750a7d9
Alain Hélaïli <alain@carbonhub.io>
2021-02-01 22:45:53 +0100

    define remote_branch before using it

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,6 +65,17 @@ else
   INPUT_JEKYLL_ENV="production"
 fi  
 
+if [ -n "${INPUT_TARGET_BRANCH}" ]; then
+  remote_branch="${INPUT_TARGET_BRANCH}"
+  echo "::debug::target branch is set via input parameter"
+else
+  # Is this a regular repo or an org.github.io type of repo
+  case "${GITHUB_REPOSITORY}" in
+    *.github.io) remote_branch="master" ;;
+    *)           remote_branch="gh-pages" ;;
+  esac
+fi
+
 REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 echo "::debug::Remote is ${REMOTE_REPO}"
 BUILD_DIR="${GITHUB_WORKSPACE}/../jekyll_build"
@@ -90,17 +101,6 @@ if [ "${JEKYLL_DEBUG}" = true ]; then
   VERBOSE="--verbose"
 else 
   echo "::debug::Jekyll debug is off"
-fi
-
-if [ -n "${INPUT_TARGET_BRANCH}" ]; then
-  remote_branch="${INPUT_TARGET_BRANCH}"
-  echo "::debug::target branch is set via input parameter"
-else
-  # Is this a regular repo or an org.github.io type of repo
-  case "${GITHUB_REPOSITORY}" in
-    *.github.io) remote_branch="master" ;;
-    *)           remote_branch="gh-pages" ;;
-  esac
 fi
 
 JEKYLL_ENV=${INPUT_JEKYLL_ENV} bundle exec ${BUNDLE_ARGS} jekyll build -s ${GITHUB_WORKSPACE}/${JEKYLL_SRC} -d ${BUILD_DIR} ${INPUT_JEKYLL_BUILD_OPTIONS} ${VERBOSE} 
