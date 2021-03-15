5cae77c07b0c8344338c35e26c8a75077b081f88
Alain Hélaïli <alain@carbonhub.io>
2021-03-15 16:08:41 +0100

    adding the simple path test

--- tests/e2e/cypress/integration/multiple/spec.js
+++ tests/e2e/cypress/integration/multiple/spec.js
@@ -4,10 +4,8 @@ it('works for the default branch', () => {
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Multiple versions')
 })
 
-/*
 it('works for the default branch on a different path', () => {
   cy.visit('/jekyll-action/current')
   cy.get('#multiple').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Multiple versions')
 })
-*/
