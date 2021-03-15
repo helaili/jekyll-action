01d77087e617368c19814bc9f645ba04f5082da5
Alain Hélaïli <alain@carbonhub.io>
2021-03-15 16:18:38 +0100

    publish old version too

--- tests/e2e/cypress/integration/multiple/spec.js
+++ tests/e2e/cypress/integration/multiple/spec.js
@@ -9,3 +9,9 @@ it('works for the default branch on a different path', () => {
   cy.get('#multiple').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Multiple versions')
 })
+
+it('works for the default branch on a different path', () => {
+  cy.visit('/jekyll-action/old')
+  cy.get('#multiple').should('be.visible')
+  cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Multiple versions but older')
+})
