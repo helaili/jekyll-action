65390a6d52d2358121560dda363fa72d9903f46a
Malachi Soord <inverse.chi@gmail.com>
2022-01-27 09:27:46 +0100

    Remove debug line

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ if [ -n "$INPUT_PRE_BUILD_COMMANDS" ]; then
   eval "$INPUT_PRE_BUILD_COMMANDS"
 fi 
 
-echo "INPUT_BUNDLER_VERSION:${INPUT_BUNDLER_VERSION}"
-
 if [ -n "$INPUT_BUNDLER_VERSION" ]; then
   echo "Installing bundler version specified by the user."
   gem install bundler -v $INPUT_BUNDLER_VERSION
