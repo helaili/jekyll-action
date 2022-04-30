5a5368c1065a328674c855e868a0020d0103c428
Malachi Soord <inverse.chi@gmail.com>
2022-04-30 21:12:49 +0200

    Additional

---
+++ tests/e2e/cypress/integration/jekyll_bundler/spec.js
@@ -0,0 +1,6 @@
+it('works', () => {
+  cy.visit('/jekyll-action/')
+  cy.get('#jekyll_src').should('be.visible')
+  cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Jekyll Bundler')
+  cy.get('#env').should('contain', 'production')
+})
