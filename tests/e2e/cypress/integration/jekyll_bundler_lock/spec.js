58c84ca170a5c68fddf6760978666f5010fd0c2b
Malachi Soord <inverse.chi@gmail.com>
2022-05-01 22:05:01 +0200

    Fix specs

--- tests/e2e/cypress/integration/jekyll_bundler_lock/spec.js
+++ tests/e2e/cypress/integration/jekyll_bundler_lock/spec.js
@@ -1,6 +1,6 @@
 it('works', () => {
   cy.visit('/jekyll-action/')
-  cy.get('#jekyll_src').should('be.visible')
+  cy.get('#jekyll_bundler_lock').should('be.visible')
   cy.get('body main div header h1').should('contain', 'Jekyll AsciiDoc Action - Jekyll Bundler Lock')
   cy.get('#env').should('contain', 'production')
 })
